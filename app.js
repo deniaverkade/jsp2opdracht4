da0a971783f2fecae150eb23a6ddf8c289b4f881
deniaverkade <9024406@student.zadkine.nl>
2024-12-12 18:36:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+//opdracht 1 
+
+const tafelDiv = document.getElementById('tafel');
+
+
+for (let i = 1; i <= 10; i++) {
+
+    const vermenigvuldiging = `10 × ${i} = ${10 * i}`;
+    
+    tekst = document.getElementById("tafel")
+    tekst.textContent = vermenigvuldiging;
+
+
+    tafelDiv.appendChild(p);
+}
+
+
+// ik heb online geprobeerd te kijken voor hulp maar ik kom er eerlijk gezegd niet uit met hoe ik alle tafels op het scherm te zien krijg
